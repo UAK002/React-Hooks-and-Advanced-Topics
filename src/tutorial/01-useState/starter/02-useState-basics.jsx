e32cdea96c3a71e17507417c4ded6239b565c2ec
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-24 11:49:32 +0530

    79. UseState Fundamentals

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState());
+  // console.log(useState('bob'));
+
+  // const value = useState('hello')[0];
+  // const func = useState('hello')[1];
+  // console.log(value);
+  // console.log(func);
+
+  // const [count, setCount] = useState(0);
+  // console.log(count);
+  // console.log(setCount);
+
+  const [count, setCount] = useState(0);
+
+  const handClick = () => {
+    setCount(count + 1);
+    // setCount('pants');
+  };
+
+  return (
+    <div>
+      <h2>You click {count} times</h2>
+      <button type="button" className="btn" onClick={handClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
