31b9def54c6b0cd4446e8a998fb92af8d77d5be7
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-26 13:13:34 +0530

    87. UseState Object Example - Complete

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,35 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('peter');
-  const [age, setAge] = useState(24);
-  const [hobby, setHobby] = useState('Reading books');
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    name: 'reading books',
+  });
+
+  // const [name, setName] = useState('peter');
+  // const [age, setAge] = useState(24);
+  // const [hobby, setHobby] = useState('Reading books');
 
   const displayPerson = () => {
-    setName('john');
-    setAge(28);
-    setHobby('screaming at the computer');
+    // setName('john');
+    // setAge(28);
+    // setHobby('screaming at the computer');
+
+    // setPerson({ name: 'john', age: 28, hobby: 'screaming at the computer' });
+    // setPerson('shakeAndBake');
+    // setPerson({ name: 'john' });
+    setPerson({ ...person, name: 'john' });
   };
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h3>Enjoys: {hobby}</h3>
+      {/* <h3>{name}</h3> */}
+      <h3>{person.name}</h3>
+      {/* <h3>{age}</h3> */}
+      <h3>{person.age}</h3>
+      {/* <h3>Enjoys: {hobby}</h3> */}
+      <h3>Enjoys: {person.hobby}</h3>
       <button className="btn" onClick={displayPerson}>
         show john
       </button>
