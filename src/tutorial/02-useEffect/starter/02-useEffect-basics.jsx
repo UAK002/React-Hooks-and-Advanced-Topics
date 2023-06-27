32149600072d18cc2bd58c88b4a346ed28a6b115
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-27 21:54:29 +0530

    93. UseEffect - Fundamentals

--- src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
@@ -8,10 +8,20 @@ const UseEffectBasics = () => {
 
   sayHello();
 
+  useEffect(() => {
+    console.log('hello from useEffect');
+  }, []);
+  // useEffect(() => {
+  //   const someFunc = async () => {
+  //     await fetch;
+  //   };
+  //   someFunc();
+  // }, []);
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
