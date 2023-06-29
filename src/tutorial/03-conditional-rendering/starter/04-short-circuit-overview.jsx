8eac0c68f2bb0e505d820dec1d395400f795debc
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-29 20:50:51 +0530

    106. Short Circuit - Overview

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,23 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  // falsy
+  const [text, setText] = useState('');
+
+  // truthy
+  const [name, setName] = useState('susan');
+
+  const codeExample = text || 'hello World';
+
+  return (
+    <div>
+      {/* {if(name){console.log("hello")}} */}
+      <h4>Falsy OR : {text || 'hello world'}</h4>
+      <h4>Falsy AND : {text && 'hello world'}</h4>
+      <h4>Truthy OR : {text || 'hello world'}</h4>
+      <h4>Truthy AND : {text && 'hello world'}</h4>
+      {codeExample}
+    </div>
+  );
 };
 export default ShortCircuitOverview;
