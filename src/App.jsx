77a028baa9ab516968bf96ada01d91ba9b4a4fe8
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-06 19:06:30 +0530

    118. Project Structure - Export Group

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Text } from './text';
+import { Text } from '../src/tutorial/04-project-structure/Text';
 import Example from './tutorial/04-project-structure/starter/Example';
 
 function App() {
