ee971dda399e3bdab4b9638760c8ae748cbfb9fe
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-06 21:23:43 +0530

    119. Leverage Javascript - Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { Text } from '../src/tutorial/04-project-structure/Text';
-import Example from './tutorial/04-project-structure/starter/Example';
-
 function App() {
   return (
     <div className="container">
-      <Example />
-      <Text />
+      <h2>leverage javascript</h2>
     </div>
   );
 }
