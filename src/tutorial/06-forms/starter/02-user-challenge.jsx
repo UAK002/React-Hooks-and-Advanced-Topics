8a90822629581a16e3420f700bb3ca9724b427d4
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-08 01:41:02 +0530

    125. User Challenge - Add New User

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,62 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const fakeId = Date.now();
+
+    // console.log(fakeId);
+    const newUser = { id: fakeId, name };
+
+    const updatedUsers = [...users, newUser];
+
+    setUsers(updatedUsers);
+
+    setName('');
+
+    // console.log('form submitted');
+    // console.log(users);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <h2>User</h2>
+      {/* {data.map((user) => { */}
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
