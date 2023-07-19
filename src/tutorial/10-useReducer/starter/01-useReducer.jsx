fa67ebf0658d7c599173eed16dbf2a7b2b724bc2
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-19 17:29:56 +0530

    146. UseReducer - Setup

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { data } from '../../../data';
+import { useReducer } from 'react';
 
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const defaultState = {
+    people: data,
+  };
+
+  const reducer = () => {};
+
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
 
   const clearList = () => {
-    setPeople(data);
+    // setPeople([]);
   };
+  const resetList = () => {
+    // setPeople(data);
+  };
+
+  console.log(state);
 
   return (
     <div>
-      {people.map((person) => {
+      {/* {people.map((person) => { */}
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -24,11 +39,12 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length < 1 ? (
+      {/* {people.length < 1 ? ( */}
+      {state.people.length < 1 ? (
         <button
           className="btn"
           style={{ marginTop: '2rem' }}
-          onClick={clearList}
+          onClick={removeItem}
         >
           reset
         </button>
@@ -36,7 +52,7 @@ const ReducerBasics = () => {
         <button
           className="btn"
           style={{ marginTop: '2rem' }}
-          onClick={() => setPeople([])}
+          onClick={clearList}
         >
           clear items
         </button>
