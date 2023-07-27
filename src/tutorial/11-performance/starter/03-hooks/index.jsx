0facbc710cc7f81e02fa4c575f95bc7295eaa17d
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-27 16:28:21 +0530

    162. UseMemo Hook

--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,16 +1,20 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { data } from '../../../../data';
 import List from './List';
+import slowFunction from './slowFunction';
+
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+
+  const value = useMemo(() => slowFunction(), []);
+  console.log(value);
 
   const removePerson = useCallback(
     (id) => {
       console.log(people);
       const newPeople = people.filter((person) => person.id !== id);
       setPeople(newPeople);
-      // }, []);
     },
     [people]
   );
